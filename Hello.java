1765d99088ad0b1b577e9a5a4466ca47a355bdfe
unknown <sskur@msn.com>
2020-01-28 15:18:51 +0530

    [JRA-123] fix typo

--- Hello.java
+++ Hello.java
@@ -1,11 +1,7 @@
 class Hello {
 
-public static void main (String args[])
-
-{
-
-System.out.println("Java Hello World");
-
+public static void main (String args[]){
+   System.out.println("Java Hello World");
 }
 
 }
